58555bc28d8b002537507d2b5a3f69c1abd39c30
ltroberts <ltroberts@crimson.ua.edu>
2023-09-29 14:47:05 -0500

    Finished the Checkers tutorial from youtube

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
-from checkers.constants import WIDTH, HEIGHT, ROWS, COLS, SQUARE_SIZE
-from checkers.board import Board
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.game import Game
+
+FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
-FPS = 60
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
 
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+            
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
 
-        board.draw(WIN)
-        pygame.display.update()
-
+        game.update()
+    
     pygame.quit()
 
-if __name__ == "__main__":
-    main()
+main()
