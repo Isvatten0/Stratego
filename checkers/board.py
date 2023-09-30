e80e6f03f9400c08b7fe542974ed1800dd37fcc6
ltroberts <ltroberts@crimson.ua.edu>
2023-09-30 00:18:01 -0500

    name change and add more images constants

--- checkers/board.py
+++ checkers/board.py
@@ -33,15 +33,15 @@ class Board:
         for row in range(ROWS):
             self.board.append([])
             for col in range(COLS):
-                if col % 2 == ((row +  1) % 2):
+                # if col % 2 == ((row +  1) % 2):
                     if row < 3:
                         self.board[row].append(Piece(row, col, WHITE))
                     elif row > 4:
                         self.board[row].append(Piece(row, col, RED))
                     else:
                         self.board[row].append(0)
-                else:
-                    self.board[row].append(0)
+                # else:
+                #     self.board[row].append(0)
         
     def draw(self, win):
         self.draw_squares(win)
